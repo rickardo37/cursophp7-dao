c71d4990c12c217e3cf6e8217d8c578a6647e01a
Ricardo Barbosa <rick.barbosa2909@gmail.com>
2018-08-07 21:17:21 -0300

    Inserção e Edição de Registro

--- index.php
+++ index.php
@@ -1,26 +1,47 @@
 <?php
 require_once("config.php");
-//CARREGA UM USUARIO
+//CARREGA UM USUARIO PELO SEU ID#####################
 //$root = new Usuario();
-//$root->loadById(1);
+//$root->loadById(12);
 //echo $root;
+//####################################################
 
 
-// CARREGA UMA LISTA DE USUARIOS
+// CARREGA UMA LISTA DE USUARIOS######################
 //$lista = Usuario::getList();
 //echo json_encode($lista);
+//####################################################
 
-//CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN
+
+//CARREGA UMA LISTA DE USUARIOS BUSCANDO PELO LOGIN###
 //$search = Usuario::search("ka");
 //echo json_encode($search);
+//####################################################
 
-//CARREGA UM USUARIO USANDO O LOGIN E A SENHA
+
+//CARREGA UM USUARIO USANDO O LOGIN E A SENHA#########
+//$usuario = new Usuario();
+//$usuario->login("karlos","123rtt");
+//echo $usuario;
+//####################################################
+
+
+
+//INSERÇÃO NO BANCO###################################
+//$aluno = new Usuario("JUMJE","KJHGFT");
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@weert");
+
+//$aluno->insert();
+
+//echo $aluno;
+//#####################################################
+
+//FAZ UMA EDIÇÃO DE UM REGISTRO
 $usuario = new Usuario();
-$usuario->login("karlos","123rtt");
-
+$usuario->loadById(13);
+$usuario->update("professor","rreeww");
 echo $usuario;
-
-
 
 
 
